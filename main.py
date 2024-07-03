081751456f7fc8f0e4d527f371fd52b06f5f0f68
Alexander Carrasquilla <62528508+GnnSAlexander@users.noreply.github.com>
2024-07-03 22:38:59 +0000

    day3_100days

--- main.py
+++
@@ -1,7 +0,0 @@
-food = input("Name a food >")
-plant = input("Name a type of plant >")
-cooking = input("Name a method of cooking >")
-burned = input("What word describes burned food? >")
-item = input("Name a DIY item >")
-
-print(cooking ," ", food, "with ", burned, " ", plant, " on a bed of ", item)
